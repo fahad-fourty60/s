5a57968cc51970587638796f4680d49f62a5b259
fahad-fourty60 <fahad@pentagoat.com>
2024-09-16 15:06:07 +0530

    commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// nav section js start //
+
+
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".open-menu-btn");
+const closeMenuBtn = document.querySelector(".close-menu-btn");
+
+[openMenuBtn, closeMenuBtn].forEach((btn) => {
+    btn.addEventListener('click', () => {
+        menu.classList.toggle('open');
+        menu.style.transition = "transform 0.5s ease";
+    });
+});
+menu.addEventListener('transitionend', function () {
+    this.romoveAttribute('style');
+});
+menu.querySelectorAll('.dropdown > i').forEach((arrow) => {
+    arrow.addEventListener('click', function () {
+        this.closest('.dropdown').classList.toggle('active');
+    });
+});
+
+
+// <!-- nav section js end -->// 
